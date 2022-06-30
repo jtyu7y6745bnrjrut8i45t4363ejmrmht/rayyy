03bd7bd54c3549839bdaed5b7deb7cc283e15f38
jtyu7y6745bnrjrut8i45t4363ejmrmht <100298421+jtyu7y6745bnrjrut8i45t4363ejmrmht@users.noreply.github.com>
2022-06-30 09:34:41 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # configs
-AUUID=813c7144-9f76-4151-979e-692668d8f0e7
+AUUID=f24bc904-fe6e-4e5c-b3d7-4a2bd975fa8b
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/kaiedkf23/task/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/kaiedkf23/task/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/jtyu7y6745bnrjrut8i45t4363ejmrmht/rayyy/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/jtyu7y6745bnrjrut8i45t4363ejmrmht/rayyy/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/kaiedkf23/task/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/jtyu7y6745bnrjrut8i45t4363ejmrmht/rayyy/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
